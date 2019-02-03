23cf32fa423f8af9cc975ccee7a84a7c24a7c02a
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-03 19:21:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -41,33 +41,6 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener (tweets_array)
-  short_tweet_array = []
-  tweets_array.each do |tweet|
-    words = tweet.split(" ")
-    short_tweet = []
-    
-    words.each do |word|
-      case word.downcase
-      when "hello"
-        short_tweet << "hi"
-      when "to", "two", "too"
-        short_tweet << "2"
-      when "for", "four" 
-        short_tweet << "4"
-      when "be" 
-        short_tweet << "b"
-      when "you" 
-        short_tweet << "u"
-      when "at" 
-        short_tweet << "@" 
-      when "and" 
-        short_tweet << "&"
-      else
-        short_tweet << word
-      short_tweet_array << short_tweet.join(" ")
-    end
-  end
- end
- short_tweet_array
+  tweets_array.collect
 end
 
